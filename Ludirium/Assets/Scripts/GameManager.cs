f179d0b3a00d483c2aaf9f29d16cec6ac7bb7b0c
Kyle Chu <ktnc.985@gmail.com>
2018-12-07 19:07:15 +0800

    add random breaking and random parts needed for Component controller

--- Ludirium/Assets/Scripts/GameManager.cs
+++ Ludirium/Assets/Scripts/GameManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public GameObject[] componentList;
+    public float energyStored;
+    public float decayRate;
 
     private int updateTime = 1;
 
 	// Use this for initialization
 	void Start () {
-		
+        energyStored = 100f;
+        decayRate = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(energyStored <= 0)
+        {
+            
+        }
         if(Time.time>= updateTime)
         {
             updateTime = Mathf.FloorToInt(Time.time) + 1;
@@ -22,7 +29,13 @@ public class GameManager : MonoBehaviour {
             {
                 component.GetComponent<ComponentController>().decay();
             }
+            energyStored -= decayRate;
         }
 		
 	}
+
+    public void windUp()
+    {
+        energyStored = 100f;
+    }
 }
