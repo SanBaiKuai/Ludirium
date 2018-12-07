a31ebe1f49bc31a01f4b5b0cedc5d42dfd281c3a
khooroko <khoowp25@hotmail.com>
2018-12-07 16:20:53 +0800

    Fix

---
+++ Ludirium/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+    public static CanvasManager Instance {get; private set; }
+    public Text bottomLeftText;
+    public Text currTime;
+    public Text bestTime;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		if (PlayerPrefs.HasKey("bestTime")) {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowBottomLeftText() {
+        bottomLeftText.enabled = true;
+    }
+
+    public void ShowBottomLeftText(string msg) {
+        bottomLeftText.text = msg;
+        ShowBottomLeftText();
+    }
+
+    public void HideBottomLeftText() {
+        bottomLeftText.enabled = false;
+    }
+}
---
+++ Ludirium/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool canInteract = false;   // true if currently can interact with some object
+    GameObject interactable;
+    GameObject[] inventory = new GameObject[3];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Component") {
+            canInteract = true;
+            interactable = other.gameObject;
+            CanvasManager.Instance.ShowBottomLeftText("Press Q to repair");
+        } else if (other.tag == "Item") {
+            canInteract = true;
+            interactable = other.gameObject;
+            CanvasManager.Instance.ShowBottomLeftText("Press Q to pick up");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (canInteract) {
+            canInteract = false;
+            interactable = null;
+            CanvasManager.Instance.HideBottomLeftText();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (canInteract && Input.GetKeyDown(KeyCode.Q)) {
+            // do the interaction shit here
+            //if (interactable.GetComponent<> != null) {
+
+            //} else if (interactable.GetComponent<Item> != null) {
+
+            //}
+        }
+	}
+}
