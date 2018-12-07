2e53fe431a593e73e49422fb32177ee2b646d284
khooroko <khoowp25@hotmail.com>
2018-12-08 02:29:19 +0800

    Add monkey break and fix behaviour

--- Ludirium/Assets/Scripts/Monkey.cs
+++ Ludirium/Assets/Scripts/Monkey.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Monkey : MonoBehaviour {
 
     private Animator anim;
     private ComponentController cc;
+    public AudioMixer am;
+    bool isBroken;
 
     // Use this for initialization
     void Start () {
@@ -15,22 +18,38 @@ public class Monkey : MonoBehaviour {
 
     void Update()
     {
-        if (cc.isBroken)
-        {
-            Break();
+        if (cc.isBroken != isBroken) {
+            if (cc.isBroken) {
+                Break();
+            } else {
+                Fix();
+            }
+            isBroken = cc.isBroken;
         }
-        else
-        {
-            Fix();
-        }
-
     }
 
     public void Break() {
         anim.SetBool("Broken", true);
+        StartCoroutine(TurnDown());
     }
 
     public void Fix() {
         anim.SetBool("Broken", false);
+        StartCoroutine(TurnUp());
+    }
+
+    IEnumerator TurnUp() {
+        for (int i = 9; i >= 0; i--) {
+            am.SetFloat("BGMVol", i * (-5));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator TurnDown() {
+        for (int i = 0; i < 20; i++) {
+            am.SetFloat("BGMVol", i * (-2.5f));
+            yield return new WaitForSeconds(0.1f);
+        }
+        am.SetFloat("BGMVol", -80f);
     }
 }
