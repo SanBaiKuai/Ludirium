56c44a3623f27a08545faa88157accb94eb318c1
khooroko <khoowp25@hotmail.com>
2018-12-07 18:50:31 +0800

    Improve picking up

---
+++ Ludirium/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+    public static ItemManager Instance { get; private set; }
+    public GameObject gear;
+    public GameObject spring;
+    public GameObject screw;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Ludirium/Assets/Scripts/PlayerController.cs
+++ Ludirium/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private static readonly int MAX_ITEMS_HELD = 3;
     public bool canInteract = false;   // true if currently can interact with some object
+    public bool isFullLoad = false; // true if currently holding max number of items 
     GameObject interactable;
     //public GameObject[] inventory = new GameObject[3];
-    public Statics.Items[] inventory = new Statics.Items[3];
+    public Statics.Items[] inventory = new Statics.Items[MAX_ITEMS_HELD];
     public Transform[] spawnPoints;
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < inventory.Length; i++) {
+            inventory[i] = Statics.Items.NONE;
+        }
 	}
 
     private void OnTriggerEnter(Collider other) {
@@ -22,8 +26,12 @@ public class PlayerController : MonoBehaviour {
             CanvasManager.Instance.ShowBottomLeftText("Press Q to repair");
         } else if (other.tag == "Factory") {
             canInteract = true;
-            interactable = other.gameObject;
-            CanvasManager.Instance.ShowBottomLeftText("Press Q to pick up");
+            if (isFullLoad) {
+                CanvasManager.Instance.ShowBottomLeftText("Inventory full!");
+            } else {
+                interactable = other.gameObject;
+                CanvasManager.Instance.ShowBottomLeftText("Press Q to pick up");
+            }
         }
     }
 
@@ -40,8 +48,30 @@ public class PlayerController : MonoBehaviour {
 		if (canInteract && Input.GetKeyDown(KeyCode.Q)) {
             // do the interaction shit here
             if (interactable.tag == "Factory") {
-                for (int i = 0; i < 3; i++) {
-                    if (inventory[i] == null) {
+                for (int i = 0; i < MAX_ITEMS_HELD; i++) {
+                    if (inventory[i] == Statics.Items.NONE) {
+                        inventory[i] = interactable.GetComponent<Factory>().item;
+                        GameObject newItem = null;
+                        switch (inventory[i]) {
+                            case Statics.Items.NONE:
+                                break;
+                            case Statics.Items.GEAR:
+                                newItem = Instantiate(ItemManager.Instance.gear);
+                                break;
+                            case Statics.Items.SPRING:
+                                newItem = Instantiate(ItemManager.Instance.spring);
+                                break;
+                            case Statics.Items.SCREW:
+                                newItem = Instantiate(ItemManager.Instance.screw);
+                                break;
+                        }
+                        if (newItem != null) {
+                            newItem.transform.parent = spawnPoints[i].transform;
+                            newItem.transform.position = spawnPoints[i].transform.position;
+                        }
+                        if (i == MAX_ITEMS_HELD - 1) {
+                            isFullLoad = true;
+                        }
                         break;
                     }
                 }
