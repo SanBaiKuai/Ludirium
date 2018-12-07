1bb7a4f7a58e686a0cc80a15902cc48ee2e5e43e
Kyle Chu <ktnc.985@gmail.com>
2018-12-07 16:12:49 +0800

    add units breaking

---
+++ Ludirium/Assets/Scripts/ComponentController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentController : MonoBehaviour {
+    public enum Items {None, Gear, Spring, Tape };
+    public float decayRate;
+    public float health;
+    public Items repairItem;
+
+    private bool isBroken;
+
+	// Use this for initialization
+	void Start () {
+        isBroken = false;
+        health = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    //returns true if repair is successful, returns 0 otherwise
+    public bool repair(Items item)
+    {
+        if (item == repairItem)
+        {
+            isBroken = false;
+            health = 100f;
+            return true;
+        } else if (item == Items.Tape)
+        {
+            isBroken = false;
+            health = 50f;
+            return true;
+        }
+        return false;
+    }
+
+    public void decay()
+    {
+        if (!isBroken)
+        {
+            health -= decayRate;
+            if (health <= 0)
+            {
+                health = 0;
+                isBroken = true;
+            }
+        }
+    }
+}
---
+++ Ludirium/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] componentList;
+
+    private int updateTime = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time>= updateTime)
+        {
+            updateTime = Mathf.FloorToInt(Time.time) + 1;
+            foreach (GameObject component in componentList)
+            {
+                component.GetComponent<ComponentController>().decay();
+            }
+        }
+		
+	}
+}
